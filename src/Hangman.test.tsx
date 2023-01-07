c1698e8124cfc47ee06025f78cb2553d4e83dcd5
DerkTerk <derektang101@gmail.com>
2023-01-07 15:01:04 -0500

    revived the hangman by adding images

--- src/Hangman.test.tsx
+++ src/Hangman.test.tsx
@@ -1,62 +1,57 @@
-import React from 'react';
-import { fireEvent, render, screen, within } from '@testing-library/react';
-import App from './App';
-import userEvent from '@testing-library/user-event'
-import Hangman from './Hangman';
+import React from "react";
+import {render, screen, within} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Hangman from "./Hangman";
+
 describe("hangman", () => {
-	it("renders given word", () => {
-    render(<Hangman currentWord = {'pineapple'} />)
-    expect(screen.getByText('pineapple')).toBeVisible()
-  })
-})
+    // test("renders given word", () => {
+    //   render(<Hangman currentWord={"pineapple"} />);
+    //   expect(screen.getByText("pineapple")).toBeVisible();
+    // });
 
+    it("elements render", () => {
+        render(<Hangman currentWord=""/>);
+        expect(input()).toBeVisible();
+        expect(getSubmitButton()).toBeVisible();
+    });
+    it("input() updates graveyard", () => {
+        render(<Hangman currentWord=""/>);
+        userEvent.type(input(), "z");
+        userEvent.click(getSubmitButton());
+        expect(screen.getByText("z")).toBeVisible();
+    });
+    it("input() takes one value", () => {
+        render(<Hangman currentWord=""/>);
+        userEvent.type(input(), "asdfasdfasdf");
+        expect(getSubmitButton()).toBeDisabled();
+    });
+    it("no repeat inputs", () => {
+        render(<Hangman currentWord=""/>);
+        userEvent.type(input(), "z");
+        userEvent.click(getSubmitButton());
+        userEvent.type(input(), "z");
+        expect(getSubmitButton()).toBeDisabled();
+    });
+    it("target word", () => {
+        render(<Hangman currentWord="at"/>);
+        const bank = screen.getByLabelText("target word");
+        userEvent.type(input(), "a");
+        userEvent.click(getSubmitButton());
+        expect(within(bank).getByText("a")).toBeVisible();
+    });
 
-
-describe('input', () => {
-	test('elements render', () => {
-		render(<Hangman currentWord='' />);
-		const input = screen.getByRole('textbox', {name: 'Letter'})
-		expect(input).toBeVisible()
-		expect(getSubmitButton()).toBeVisible()
-	});
-	test('input updates graveyard', () => {
-		render(<Hangman currentWord="" />)
-		const input = screen.getByRole('textbox', {name: 'Letter'})
-		userEvent.type(input, 'z')
-		userEvent.click(getSubmitButton())
-		expect(screen.getByText('z')).toBeVisible()
-	})
-	test('input takes one value', () => {
-		render(<Hangman currentWord="" />)
-		const input = screen.getByRole('textbox', {name: 'Letter'})
-		userEvent.type(input, 'asdfasdfasdf')
-		expect(getSubmitButton()).toBeDisabled()
-	})
-	test('no repeat inputs', () => {
-		render(<Hangman currentWord="" />)
-		const input = screen.getByRole('textbox', {name: 'Letter'})
-		userEvent.type(input, 'z')
-		userEvent.click(getSubmitButton())
-		userEvent.type(input, 'z')
-		expect(getSubmitButton()).toBeDisabled()
-	})
-  test('target word', () => {
-    render(<Hangman currentWord="at" />)
-    const input = screen.getByRole('textbox', {name: 'Letter'})
-    const bank = screen.getByLabelText('target word')
-    userEvent.type(input, 'a')
-    userEvent.click(getSubmitButton())
-    expect(within(bank).getByText('a')).toBeVisible()
-  })
+    it("winning", () => {
+        render(<Hangman currentWord="at"/>);
+        userEvent.type(input(), "a");
+        userEvent.click(getSubmitButton());
+        userEvent.type(input(), "t");
+        userEvent.click(getSubmitButton());
+        expect(screen.getByRole("presentation")).toBeVisible();
+    });
 });
 
-// describe('outcomes', () => {
-//   test('winning', () => {
-//     render(<Hangman currentWord="at" />)
-//     const win = 
-//   })
-// });
-
 function getSubmitButton() {
-  return screen.getByRole('button', { name: 'submit' });
+    return screen.getByRole("button", {name: "submit"});
 }
+
+const input = () => screen.getByRole("textbox", {name: "Letter"});
--- src/Hangman.tsx
+++ src/Hangman.tsx
@@ -1,49 +1,110 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import { WORDLIST } from "./words";
+import React, {useState} from "react";
+import {
+    Button,
+    Dialog, DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Stack,
+    TextField,
+    Typography,
+} from "@mui/material";
+
 export interface HangmanProps {
-  currentWord: string;
+    currentWord: string;
 }
 
 const Hangman = (props: HangmanProps) => {
-  const [graveyard, setGraveyard] = useState<string[]>([]);
-  const [inputValue, setInputValue] = useState('');
-  const [correctBank, setCorrectBank] = useState('')
-  const { currentWord } = props;
+    const [graveyard, setGraveyard] = useState<string[]>([]);
+    const [inputValue, setInputValue] = useState("");
+    const [correctBank, setCorrectBank] = useState("");
+    const {currentWord} = props;
+    const [remainingLetters, setRemainingLetters] = useState(currentWord)
+    const [openDialog, setOpenDialog] = useState(false)
 
-  function handleSubmit(event: React.FormEvent) {
-    event.preventDefault();
-    if (currentWord.includes(inputValue)) {
-      setCorrectBank(correctBank + inputValue)
+    function handleSubmit(event: React.FormEvent) {
+        event.preventDefault();
+        if (currentWord.includes(inputValue)) {
+            setCorrectBank(correctBank + inputValue);
+            setRemainingLetters(remainingLetters.replace(inputValue, ''))
+            if (remainingLetters.length === 0) {
+                setOpenDialog(true )
+            }
+        }
+        const newGraveyard = graveyard;
+        newGraveyard.push(inputValue);
+        setInputValue("");
+        setGraveyard(newGraveyard);
     }
-    const newGraveyard = graveyard;
-    newGraveyard.push(inputValue);
-    setInputValue("");
-    setGraveyard(newGraveyard);
-  }
-  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    setInputValue(event.target.value);
-  }
-  let isSubmitDisabled =
-    inputValue.length !== 1 || graveyard.includes(inputValue);
-  return (
-    <div>
-      <h2>{currentWord}</h2>
-      <div className="app__graveyard">{graveyard}</div>
-      <div aria-label="target word">{correctBank}</div>
-      <form onSubmit={handleSubmit}>
-        <input
-          onChange={handleChange}
-          aria-label="Letter"
-          placeholder="type one letter"
-          value={inputValue}
-        />
-        <button disabled={isSubmitDisabled} type="submit">
-          submit
-        </button>
-      </form>
-    </div>
-  );
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setInputValue(event.target.value);
+    }
+
+    let isSubmitDisabled =
+        inputValue.length !== 1 || graveyard.includes(inputValue);
+    return (
+        <div>
+            <h2>{currentWord}</h2>
+            <Stack
+                className="app__graveyard"
+                direction="row"
+                alignItems="flex-end"
+                gap={0.5}
+            >
+                <Typography variant="h6">Guessed Letters: </Typography>
+                <Typography mb="3px">{graveyard}</Typography>
+            </Stack>
+            <Stack
+                aria-label="target word"
+                alignItems="flex-end"
+                direction="row"
+                gap={0.5}
+            >
+                <Typography variant="h6">Correct Letters: </Typography>
+                <Typography mb="3px">{correctBank}</Typography>
+            </Stack>
+            <form onSubmit={handleSubmit}>
+                <Stack direction="row" gap={1}>
+                    <TextField
+                        onChange={handleChange}
+                        label="Letter"
+                        placeholder="type one letter"
+                        value={inputValue}
+                        error={inputValue.length !== 0 && isSubmitDisabled}
+                        // helperText="input must be one new letter"
+                    />
+                    <Button
+                        disabled={isSubmitDisabled}
+                        type="submit"
+                        variant="contained"
+                        size="large"
+                        onClick={handleSubmit}
+                    >
+                        submit
+                    </Button>
+                </Stack>
+            </form>
+            <img src = {`/hang${graveyard.length + 1}.PNG`} alt = "empty gallows"/>
+            <Dialog
+                open={openDialog}
+                // aria-labelledby="alert-dialog-title"
+                // aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Use Google's location service?"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Let Google help apps determine location. This means sending anonymous
+                        location data to Google, even when no apps are running.
+                    </DialogContentText>
+                </DialogContent>
+                {/*<DialogActions>*/}
+                {/*</DialogActions>*/}
+            </Dialog>
+        </div>
+    );
 };
 
 export default Hangman;
